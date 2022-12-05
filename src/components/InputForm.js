2fd4aeba5710255a68c3b9b4cb624b2d991024ae
vishal101022 <raivishal939@gmail.com>
2022-12-05 12:19:41 +0530

    adding state

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -2,13 +2,27 @@ import { useState } from "react";
 import "./InputForm.css";
 
 const InputForm = () => {
+  // adding state
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
   
   return (
     <form>
       <div className="input-form_controls">
         <div className="input-form_control">
           <label>Title</label>
-          <input type="text"  />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="input-form_control">
           <label>Amount</label>
@@ -16,7 +30,7 @@ const InputForm = () => {
             type="number"
             min="0.01"
             step="0.01"
-           
+            onChange={amountChangeHandler}
           />
         </div>
         <div className="input-form_control">
@@ -25,7 +39,7 @@ const InputForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
-            
+            onChange={dateChangeHandler}
           />
         </div>
       </div>
