e3c173808ba176a2abdb7dbe8ca3633d357cec24
vishal101022 <raivishal939@gmail.com>
2022-12-06 07:27:34 +0530

    added dropdown listener

--- src/components/ExpenseFilter.js
+++ src/components/ExpenseFilter.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import "./ExpenseFilter.css";
 
-import './ExpensesFilter.css';
+const ExpenseFilter = (props) => {
+  const dropdownChangeHandler = (event) => {
+    props.changeFilter(event.target.value);
+  };
 
-const ExpensesFilter = () => {
   return (
-    <div className='expenses-filter'>
-      <div className='expenses-filter__control'>
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select>
-          <option value='2022'>2022</option>
-          <option value='2021'>2021</option>
-          <option value='2020'>2020</option>
-          <option value='2019'>2019</option>
+        <select value={props.selected} onChange={dropdownChangeHandler}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
         </select>
       </div>
     </div>
   );
 };
 
-export default ExpensesFilter;
+export default ExpenseFilter;
